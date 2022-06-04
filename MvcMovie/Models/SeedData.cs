983306b6a1710b857d2568bbeb3e6717a4fb923b
nurufazira <nurul28fazira@gmail.com>
2022-06-04 22:06:26 +0800

    Add project files.

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                //Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return; //DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M,
+                        Description = "A chance encounter between two graduates culminates in a short-term friendship."
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 8.99M,
+                        Description = "Three parapsychologists forced out of their university funding set up shop as a unique ghost removal service in New York City."
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 9.99M,
+                        Description = "The superstars of the supernatural reunite to battle an enormous underground river of slime that threatens New York City in this hilarious sequel."
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M,
+                        Description = "A small-town sheriff enlists a drunk, a kid and an old man to help him fight off a ruthless cattle baron."
+                    }
+                );
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
